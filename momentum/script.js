8811bd06c316cde3f3434f6c6ca6a6263a9e0db9
Igor Yevtushenko <70605781+IgorYevtushenkoUA@users.noreply.github.com>
2020-10-23 00:03:26 +0300

    feat: add background-photos

--- momentum/script.js
+++ momentum/script.js
@@ -1,13 +1,21 @@
 'use strict'
 const DAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
 const MONTH = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const PHOTOS_NUMBER = 20
 
-let time_h = document.querySelector('.time-h')
-let time_d = document.querySelector('.time-d')
-let greeting = document.querySelector(".greeting")
-let username = document.querySelector(".username")
-let focus = document.querySelector(".focus")
+
+let time_h = document.querySelector('.time-h'),
+    time_d = document.querySelector('.time-d'),
+    greeting = document.querySelector(".greeting"),
+    username = document.querySelector(".username"),
+    focus = document.querySelector(".focus"),
+    btn_photos = document.querySelector(".btn-photos"),
+
+    btn_ok = document.querySelector(".btn-ok"),
+    btn_cancel = document.querySelector(".btn-cancel")
+
+let wallpaperIndex = -1
+let photos = []
 
 function showTime() {
     let today = new Date()
@@ -21,11 +29,16 @@ function showTime() {
     time_h.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
     time_d.innerHTML = `${getDayName(dayName)}<span>,</span>${(day)}<span> </span>${getMonth(month)}`;
 
-    if (document.activeElement !== document.getElementById("username")){
+    if (document.activeElement !== document.getElementById("username")) {
         getUserName()
     }
-    if (document.activeElement !== document.getElementById("focus")){
+    if (document.activeElement !== document.getElementById("focus")) {
         getFocus()
+    }
+
+    // if (min === 0 && sec === 0) {
+    if (sec === 35) {
+        // setWallpaper()
     }
 
     setTimeout(showTime, 1000);
@@ -46,79 +59,165 @@ function getMonth(mon) {
 function getUserName() {
     if (localStorage.getItem('username') === null) {
         username.textContent = '[Enter Name]'
-    }else {
+    } else {
         username.textContent = localStorage.getItem('username')
     }
 }
 
 function setUserName(e = "keypress") {
-    if (e.type === 'keypress'){
-        if (e.which == 13 || e.keyCode == 13){
-            localStorage.setItem('username',  e.target.innerText)
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('username', e.target.innerText)
             username.blur()//???
         }
-    }else {
+    } else {
         username.textContent = e.target.innerText
     }
 }
 
-function setEmptyName(e){
+function setEmptyName(e) {
     username.textContent = ""
 }
 
 function getFocus() {
     if (localStorage.getItem('focus') === null) {
         focus.textContent = '[Enter Focus]'
-    }else {
+    } else {
         focus.textContent = localStorage.getItem('focus')
     }
 }
 
 function setFocus(e = "keypress") {
-    if (e.type === 'keypress'){
-        if(e.which === 13 || e.keyCode == 13){
+    if (e.type === 'keypress') {
+        if (e.which === 13 || e.keyCode == 13) {
             localStorage.setItem('focus', e.target.innerText)
             username.blur()
         }
-    }else {
+    } else {
         focus.textContent = e.target.innerText
     }
 }
 
-function setEmptyFocus(e){
+function setEmptyFocus(e) {
     focus.textContent = ""
 }
 
-// function setBgGreet() {
-//     let today = new Date()
-//     let hour = today.getHours()
-//     // утро 6:00-12:00,
-//     if (hour >= 6 && hour < 12){
-//         document.body.style.backgroundImage =
-//             "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-//         greeting.textContent = 'Good Morning, ';
-//     }
-//     // день 12:00-18:00,
-//     if (hour >= 12 && hour < 18){
-//         document.body.style.backgroundImage =
-//             "url('./assets/images/day/')";
-//         greeting.textContent = 'Good Afternoon, ';
-//     }
-//     // вечер 18:00-24:00,
-//     if (hour >= 18 && hour <= 23){
-//         document.body.style.backgroundImage =
-//             "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-//         greeting.textContent = 'Good Evening, ';
-//         document.body.style.color = 'white';
-//     }
-//     // ночь 24:00-6:00.
-//     if (hour >= 0 && hour < 6){
-//         document.body.style.backgroundImage =
-//             "url('https://i.ibb.co/924T2Wv/night.jpg')";
-//         greeting.textContent = 'Good Evening, ';
-//         document.body.style.color = 'white';
-//     }
-// }
+
+
+
+btn_photos.addEventListener("click", function (e) {
+    let modal = document.getElementById("modal");
+    wallpaperIndex = -1
+    fillModalWindowWithPhotos()
+    modal.style.display = "block"
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+})
+
+function fillModalWindowWithPhotos() {
+    let photos_list = document.querySelector(".photos-card")
+    let photoCard = ""
+    for (let i = 0; i < photos.length; i++) {
+        photoCard += "<div class='photo-card'><img class='background-wallpaper' id='" + i + "' src='" + photos[i] + "'></div>"
+    }
+    photos_list.innerHTML = photoCard
+    let background_wallpaper = document.querySelectorAll(".background-wallpaper")
+
+    for (let i = 0; i < background_wallpaper.length; i++) {
+        let wallpaper = background_wallpaper[i]
+        wallpaper.addEventListener('click', function (e) {
+            wallpaperIndex = e.target.id
+        })
+    }
+}
+
+btn_ok.addEventListener("click", function () {
+    debugger
+    if (wallpaperIndex !== -1) {
+        changeBackgroundWallpaper(wallpaperIndex)
+        changePhotosOrder(wallpaperIndex)
+        document.getElementById("modal").style.display = "none";
+    } else
+        alert("Choose image or click cancel")
+})
+
+function changeBackgroundWallpaper(wallpaperIndex) {
+    // getImage(wallpaperIndex)
+    document.body.style.backgroundImage = `url(${photos[wallpaperIndex]})`;
+}
+function changePhotosOrder(wallpaperIndex) {
+    let temp = []
+    for (let i = wallpaperIndex; i < photos.length; i++) temp.push(photos[i])
+    for (let i = 0; i < wallpaperIndex; i++) temp.push(photos[i])
+    photos = temp
+}
+function setWallpaper() {
+    let today = new Date()
+    let hour = today.getHours()
+    let min = today.getMinutes()
+    let sec = today.getSeconds()
+    // утро 6:00-12:00,
+    if (hour >= 6 && hour < 12) {
+        greeting.textContent = 'Good Morning, ';
+    }
+    // день 12:00-18:00,
+    if (hour >= 12 && hour < 18) {
+        document.body.style.backgroundImage =
+            "url('./assets/images/day/')";
+        greeting.textContent = 'Good Afternoon, ';
+    }
+    // вечер 18:00-24:00,
+    if (hour >= 18 && hour <= 23) {
+        document.body.style.backgroundImage =
+            "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        greeting.textContent = 'Good Evening, ';
+        document.body.style.color = 'white';
+    }
+    // ночь 24:00-6:00.
+    if (hour >= 0 && hour < 6) {
+        document.body.style.backgroundImage =
+            "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        greeting.textContent = 'Good Evening, ';
+        document.body.style.color = 'white';
+    }
+
+    let rand = Math.ceil(Math.random() * 24)
+    // document.body.style.backgroundImage = `url(${photos[hour]})`;
+    document.body.style.backgroundImage = `url(${photos[rand]})`;
+
+}
+
+btn_cancel.addEventListener("click", function () {
+    document.getElementById("modal").style.display = "none";
+})
+
+
+
+
+
+/**
+ * generate random photos list
+ */
+function generateDayPhotos() {
+    let morning = "./assets/images/morning/",
+        day = "./assets/images/day/",
+        evening = "./assets/images/evening/",
+        night = "./assets/images/night/"
+    let timeslot = [morning, day, evening, night]
+
+    for (let i = 0; i < 4; i++) {
+        let randStartNum = Math.ceil(Math.random() * PHOTOS_NUMBER) + 1
+        for (let j = randStartNum; j < randStartNum + 6; j++) {
+            let index = j !== PHOTOS_NUMBER ? j % PHOTOS_NUMBER : 20
+            photos.push(timeslot[i] + addZero(index) + ".jpg")
+        }
+    }
+
+}
 
 
 username.addEventListener("keypress", setUserName)
@@ -129,4 +228,6 @@ focus.addEventListener("click", setEmptyFocus)
 showTime()
 getUserName()
 getFocus()
-// setBgGreet()
+generateDayPhotos()
+setWallpaper()
+console.log(photos)
--- momentum/test.js
+++ momentum/test.js
@@ -1,9 +1,1 @@
-function getDayName( locale)
-{
-    var date = new Date();
-    return date.toLocaleDateString(locale, { weekday: 'long' });
-}
-
-var dateStr = '10/28/2020';
-var day = getDayName("nl-NL");
-console.log(day)
+console.log(21%20)
